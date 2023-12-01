f35b796ccc69c9bc746a6246dc87d4e1018ac1ad
clarewangari <wanjikuclare82@gmail.com>
2023-12-01 12:12:26 +0300

    worked on login testing

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,21 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
+import { UserService } from '../services/user.service';
 import { LoginComponent } from './login.component';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let loginServiceSpy: jasmine.SpyObj<LoginService>;
+  let userServiceSpy: jasmine.SpyObj<UserService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
+    loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
+    userServiceSpy = jasmine.createSpyObj('UserService', ['checkDetails']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      providers: [
+        { provide: LoginService, useValue: loginServiceSpy },
+        { provide: UserService, useValue: userServiceSpy },
+        { provide: Router, useValue: routerSpy },
+      ],
     });
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should handle successful login and navigate to the appropriate route', async () => {
+
+    const userData = { email: 'test@example.com', password: 'testpass' };
+    loginServiceSpy.login.and.returnValue(of({ message: 'Logged in successfully' }));
+    userServiceSpy.checkDetails.and.returnValue(of('customer'));
+
+
+    await component.onSubmit();
+
+
+    expect(component.loggedInState).toBe(false);
+    expect(component.loggedIn).toBe(true);
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['customer']);
+  });
+
+  it('should handle login error and display error message', async () => {
+
+    const userData = { email: 'clare@gmail.com', password: '@Qwerty123' };
+    loginServiceSpy.login.and.returnValue(of({ error: 'User details not found' }));
+
+
+    await component.onSubmit();
+
+
+    expect(component.errorMessage).toBe('Invalid credentials');
+    expect(component.loggingIn).toBe(false);
 });
